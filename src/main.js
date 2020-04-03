29304384b7ff48b2f7ffb50e134bcefd8dba3335
Serg <salekseev@mvs.group>
2020-04-03 09:12:26 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,30 @@
+//import App from './App.svelte';
+
+const L = window.L;
+const map = L.map(document.body, {
+	center: [55.758031, 37.611694],
+	minZoom: 1,
+	zoom: 8,
+	maxZoom: 21,
+	// zoomControl: false,
+	attributionControl: false,
+	trackResize: true,
+	fadeAnimation: true,
+	zoomAnimation: true,
+	distanceUnit: 'auto',
+	squareUnit: 'auto',
+});
+var baseLayers = {
+	OpenStreetMap: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	}).addTo(map)
+}
+var overlays = {
+	Marker: L.marker([55.758031, 37.611694])
+		.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+		.openPopup(),
+};
+
+L.control.layers(baseLayers, overlays).addTo(map)
+
+export default map;
